cab2452b56e093616e28d48d4d46f5a9b8236694
Shelby Sakamoto <megashelby@gmail.com>
2022-05-02 01:09:10 -0700

    graph fix v1

--- client/src/components/GraphVisuals/Graph/Graph.js
+++ client/src/components/GraphVisuals/Graph/Graph.js
@@ -16,79 +16,24 @@ export default function Graph({stockURL}) {
     "Six months": "6M",
     "YTD" : "YTD"
   }
-
-  const oneMonthData = {
-    name: "One month",
-    color: "#B6D0E2",
-    items: stock != "" ? 
-      stock.oneMonth['historical_data'].map((d) => ({ ...d, date: new Date(d.date) })) :
-      portfolio['historical_data'].map((d) => ({ ...d, date: new Date(d.date) }))
-  };
-
-  const threeMonthsData = {
-    name: "Three months",
-    color: "#CCCCFF",
-    items: stock != "" ? 
-      stock.threeMonth['historical_data'].map((d) => ({ ...d, date: new Date(d.date) })) :
-      portfolio['historical_data'].map((d) => ({ ...d, date: new Date(d.date) }))
-  };
-
-  const sixMonthsData = {
-    name: "Six months",
-    color: "#89CFF0",
-    items: stock != "" ? 
-      stock.sixMonth['historical_data'].map((d) => ({ ...d, date: new Date(d.date) })) :
-      portfolio['historical_data'].map((d) => ({ ...d, date: new Date(d.date) }))
-  };
-
-  const ytdData = {
-    name: "YTD",
-    color: "grey",
-    items: stock != "" ? 
-      stock.ytd['historical_data'].map((d) => ({ ...d, date: new Date(d.date) })) :
-      portfolio['historical_data'].map((d) => ({ ...d, date: new Date(d.date) }))
-  };
-
-  const [selectedItems, setSelectedItems] = React.useState(["One month"]);
-  const legendData = [oneMonthData, threeMonthsData, sixMonthsData, ytdData];
-  const chartData = [
-    ...[oneMonthData, threeMonthsData, sixMonthsData, ytdData].filter((d) => selectedItems.includes(d.name))
-  ];
-
-  const onChangeSelection = (name) => {
-    setSelectedItems([name]);
-  };
-
   useEffect(() => {
     const fetchStock = async () => {
       // Below will be YTD
-      const stockFromServer = await axios.get(stockURL);
-      // Calculating the day a month ago
-      var d = new Date();
-      var m = d.getMonth();
-      d.setMonth(d.getMonth() - 1);
-      // If still in same month, set date to last day of 
-      // previous month
-      if (d.getMonth() == m) d.setDate(0);
-      d.setHours(0, 0, 0, 0);
+      const stockFromServer = await axios.get(stockURL, {
+        params : {
+          "dateRange": "1Y"
+        }
+      });
       const stockOneMonthFromServer = await axios.get(stockURL, {
         params : {
           "dateRange": "1M"
         }
       });
-      // Calculating the date three months ago
-      var dThree = new Date();
-      dThree.setMonth(dThree.getMonth() - 2);
-      dThree.setHours(0, 0, 0, 0);
       const stockThreeMonthFromServer = await axios.get(stockURL, { 
         params : {
           "dateRange": "3M"
         }
       });
-      // Calculating the date six months ago
-      var dSix = new Date();
-      dSix.setMonth(dSix.getMonth() - 5);
-      dSix.setHours(0, 0, 0, 0);
       const stockSixMonthFromServer = await axios.get(stockURL, {
         params : {
           "dateRange": "6M"
@@ -103,9 +48,51 @@ export default function Graph({stockURL}) {
     }
   fetchStock()
   }, []);
-  
+  const entryID = 0
+  const oneMonthData = {
+    name: "One month",
+    color: "#B6D0E2",
+    items: stock != "" ? 
+      stock.oneMonth['historical_data'].map((d) => ({ ...d, date: new Date(Date.parse(d.date + "T" + d.time))})) :
+      portfolio['historical_data'].map((d) => ({ ...d, date: new Date(d.date) }))
+  };
+
+  const threeMonthsData = {
+    name: "Three months",
+    color: "#CCCCFF",
+    items: stock != "" ? 
+      stock.threeMonth['historical_data'].map((d) => ({ ...d, date: Date.parse(d.date + "T" + d.time)})) :
+      portfolio['historical_data'].map((d) => ({ ...d, date: new Date(d.date) }))
+  };
+
+  const sixMonthsData = {
+    name: "Six months",
+    color: "#89CFF0",
+    items: stock != "" ? 
+      stock.sixMonth['historical_data'].map((d) => ({ ...d, date: Date.parse(d.date + "T" + d.time)})) :
+      portfolio['historical_data'].map((d) => ({ ...d, date: new Date(d.date) }))
+  };
+
+  const ytdData = {
+    name: "YTD",
+    color: "grey",
+    items: stock != "" ? 
+      stock.ytd['historical_data'].map((d) => ({ ...d, date: Date.parse(d.date + "T" + d.time)})) :
+      portfolio['historical_data'].map((d) => ({ ...d, date: new Date(d.date) }))
+  };
+
+  const [selectedItems, setSelectedItems] = React.useState(["One month"]);
+  const legendData = [oneMonthData, threeMonthsData, sixMonthsData, ytdData];
+  const chartData = [
+    ...[oneMonthData, threeMonthsData, sixMonthsData, ytdData].filter((d) => selectedItems.includes(d.name))
+  ];
+
+  const onChangeSelection = (name) => {
+    setSelectedItems([name]);
+  };
   // console.log("[GRAPH.JS]: selectedItems (date range items): ", selectedItems)
   // console.log("[GRAPH.JS]: legendData: ", legendData)
+  console.log("[GRAPH.JS]: stock: ", stock);
   console.log("[GRAPH.JS]: chartData: ", chartData)
 
   return (
